3066c6647fb732e989026ab2ea3256e9bd71f89b
ben-schneider-tech-com <schnei19@myumanitoba.ca>
2023-03-18 14:01:01 -0500

    add comments and printing

--- A2_redo.py
+++ A2_redo.py
@@ -1,7 +1,16 @@
+#Query 1
+#queryVar = ['Fraud']
+#evidenceVar = []
+#evidenceValues = []
+#hiddenVar = ['Trav']
+
+# Query 2
 queryVar = []
 evidenceVar = ['Fraud','FP','IP','CRP']
 evidenceValues = ['T', 'T','F','T']
 hiddenVar = ['Trav','OC']
+
+# Whether the query is a joint table or not
 isJoint = True;
 
 
@@ -21,7 +30,7 @@ class factor:
         return sum(self.table['value'])
     
         
-def productFactor(fact1, fact2):
+def product(fact1, fact2):
     fact1 = fact1.table.rename(columns={'value': 'factor_left'}, inplace=False)
     fact2 = fact2.table.rename(columns={'value': 'factor_right'}, inplace=False)
     new_factor = fact1.merge(fact2)
@@ -31,6 +40,10 @@ def productFactor(fact1, fact2):
 
     new_factor['value'] = new_values
     new_factor=new_factor.drop(columns=['factor_left','factor_right'])
+
+    print("\n\n\n------------ Created new factor----------------")
+    print(new_factor)
+
     return factor(table=new_factor)
 
 def containsVariable(var,factor):
@@ -42,13 +55,13 @@ def printFactorList(l):
         print(i.table.to_string())
 
     
-def observeFactor(factor, variable, value):
+def observe(factor, variable, value):
     
     tableFilter = factor.table[variable] == value
     factor.table = factor.table[tableFilter]    
     return factor
 
-def normalizedFactor(factor):
+def normalize(factor):
     columnSum = factor.table['value'].sum()
     factor.table['value'] = factor.table['value'].mul(1.0 / columnSum)
     return factor
@@ -56,7 +69,7 @@ def normalizedFactor(factor):
 def findAndObserveFactor(factorList,variable,value):
     for factor in factorList :
         if(factor.table.columns[0] == variable):
-            observeFactor(factor, variable, value)
+            observe(factor, variable, value)
     
 
     
@@ -75,6 +88,10 @@ def sumOut(factor, variable):
     
     new_factor_table['value'] = new_values
     factor.table = new_factor_table
+
+    print("\n\n\n------------ Summed out " + variable + " to create new factor ----------------")
+    print(factor.table)
+
     return factor
     
 def joins(factorList,orderedListOfHiddenVariables, joinOrder):
@@ -88,13 +105,15 @@ def joins(factorList,orderedListOfHiddenVariables, joinOrder):
 
         while(len(joinList) > 0):
             join_in = joinList.pop(0)
-            currFactor = productFactor(currFactor, join_in)
+            currFactor = product(currFactor, join_in)
 
         if var in orderedListOfHiddenVariables:
             currFactor = sumOut(currFactor,var)
 
 
+
         factorList.append(currFactor)
+
     return currFactor
 
 def getVariables():
@@ -124,25 +143,28 @@ def filterFactors(factorList,queryVar, evidenceVar, hiddenVar):
     factorList = [x for x in factorList if inQuery(x.table.columns[0],queryVar, evidenceVar, hiddenVar)]
     return factorList
 
-
-
 def inference(factorList, queryVar, evidenceVar, hiddenVar):
-    joinOrder = [x for x in variables if inQuery(x,queryVar, evidenceVar, hiddenVar)]
-    factorList = filterFactors(factorList, queryVar, evidenceVar, hiddenVar)
 
+    joinOrder = [x for x in variables if inQuery(x,queryVar, evidenceVar, hiddenVar)] # figure out join order
+    factorList = filterFactors(factorList, queryVar, evidenceVar, hiddenVar) # get rid of factors we don't need in the query
 
+    # Fix our evidence factors
     for index, evidence in enumerate(evidenceVar):
-        print("Fix " + evidenceVar[index] + " to observed value of " + evidenceValues[index])
+        print("Fixed " + evidenceVar[index] + " to observed value of " + evidenceValues[index])
         findAndObserveFactor(factorList, evidenceVar[index],evidenceValues[index])
 
-
+    #Join factors together
     result = joins(factorList, hiddenVar, joinOrder)
 
+    #If the resulting table is a conditional table (not a joint), normalize it
     if(isJoint == False):
-        result = normalizedFactor(result)
+        result = normalize(result)
 
     return result
     
 
 resultFactor = inference(factorList, queryVar, evidenceVar, hiddenVar)
+
+
+print("\n\n\n------------ Inference Result----------------")
 print(resultFactor.table)
